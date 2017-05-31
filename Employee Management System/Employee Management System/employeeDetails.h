52ceadaee65d103e9efc00653dbbbae9170cdbc1
Jayant Kumar Gope <jayantgope@yahoo.in>
2017-05-31 14:45:00 +0530

    Employee Details Header File

---
+++ Employee Management System/Employee Management System/employeeDetails.h
@@ -0,0 +1,23 @@
+#ifndef EMPLOYEEDETAILS_H
+#define EMPLOYEEDETAILS_H
+
+#include <string>
+
+/*using namespace std;
+
+class employeeDetails
+{
+	public:
+		employeeDetails(string = "", string = "", string = "");
+
+		void setEName(string);
+		string getEName() const;
+
+		void setEGender(string);
+		string getEGender() const;
+
+		void setEQualification(string);
+		string getEQualification() const;
+};
+*/
+#endif
